de7c0415741243375f926420f7828a6eeaa24e27
Digvijay Rana <digvijayrana3499@gmail.com>
2024-12-01 22:21:09 +0530

    now result list is sorted

--- Solution.java
+++ Solution.java
@@ -1,24 +1,86 @@
 
 import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.Arrays;
+import java.util.concurrent.ForkJoinPool;
+import java.util.Map;
+
 import java.util.ArrayList;
 import java.util.stream.*;
+import java.lang.instrument.Instrumentation;
 public class Solution{
 	
 	public static void main(String args[])
 	{
+		/*
+		int n=5_000;
+		System.out.println("Program Started");
 		
+		IntStream.range(0,10).forEach(e->{
+			try{
+			Thread.sleep(3000);}
+			catch(InterruptedException ex)
+		{
+			System.out.println(ex);
+		}
+			System.out.println("Program Processing");});
+	
 		
+		System.out.println("Program Ended");
+		
+		*/
+	
+		//List<String> stringlist= Arrays.asList("hat","cat","bat","rat","sat","fat");
+		
+		//stringlist.forEach(System.out::println);
+		List<EmployeeBean> employees= sequential(100);
+		Map<Department,List<String>> map =employees.stream().sorted((a,b)->a.name().compareTo(b.name())).collect(Collectors.groupingBy(EmployeeBean::department,Collectors.mapping(EmployeeBean::name,Collectors.toList())));
+		map.keySet().stream().forEach(e->{
+			System.out.println(e+" : "+map.get(e));
+		});
+		List<EmployeeBean> sortedList=sortedList(employees);
+		sortedList.forEach(System.out::println);
 		//employees.forEach(System.out::println);
 	}
-	public static List<EmployeeBean> parrallel(int k)
+	public static  List<EmployeeBean> sortedList(List<EmployeeBean> employeeList)
 	{
-		List<EmployeeBean> employees;
-		long startTime=System.currentTimeMilli();
-		List<Integer>numbers=Stream.iterate(0,n->n+1).limit(k).collect(Collectors.toList());
-		employees=numbers.parallelStream().map(e->EmployeeBean.getRandom(e+"")).collect(Collectors.toList());
-		long endTime=System.currentTimeMilli();
-		System.out.println("Time taken : " +(endTime-startTime)+" Size : "+employees.size());
+		return employeeList.stream().sorted((a,b)->b.name().compareTo(a.name())).toList();
+	}
+	public static List<EmployeeBean> parallel(int k)
+	{
+		List<EmployeeBean> employees=null;
+		long startTime=System.currentTimeMillis();
+		/*ForkJoinPool forkJoinPool = new ForkJoinPool(4); // Limit the number of threads
+employees = forkJoinPool.submit(() -> Stream.iterate(0,n->n+1).limit(k).parallel()
+                                             .map(e -> EmployeeBean.getRandom(e + ""))
+                                             .collect(Collectors.toList()))
+                        .join();*/
+		
+			// Limit the number of threads
+ Stream.iterate(0,n->n+1).limit(k).parallel().forEach(e->{
+	 try{
+	 Thread.sleep(100);}
+	catch(InterruptedException ex)
+		{
+			System.out.println(ex);
+		}
+ });
+											 
+                        
+		
+		long endTime=System.currentTimeMillis();
+		System.out.println("Time taken : " +(endTime-startTime)+"ms Size : "+(employees!=null?employees.size():0));
+		return employees;
+	}
+	public static List<EmployeeBean> sequential(int k)
+	{
+		List<EmployeeBean> employees=null;
+		long startTime=System.currentTimeMillis();
+		
+		employees=IntStream.range(0,k).mapToObj(e->EmployeeBean.getRandom(e+"")).collect(Collectors.toList());
+
+		long endTime=System.currentTimeMillis();
+		System.out.println("Time taken : " +(endTime-startTime)+"ms Size : "+(employees!=null?employees.size():0));
 		return employees;
 	}
 	/*
@@ -31,6 +93,10 @@ public class Solution{
 	{
 		throw new FileNotFoundException();
 	}
+	C:\Users\digvi\OneDrive\Desktop\simple java programs>java -Xmx8g Solution
+Time taken : 641781ms Size : 50000000
+Time taken : 72943ms Size : 50000000
 	*/
-	
+	//
+	//
 }
